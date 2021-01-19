73aafe3ea8c55a294291e287026fbd5bf2e9ff37
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-01-19 16:10:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+def sort_array_asc(num)
+  num.sort do |a, b|
+    a <=> b 
+  end
+end
+
+def sort_array_desc(num)
+  num.sort do |a, b|
+    b <=> a 
+  end
+end
+
+def sort_array_char_count(num)
+  num.sort do |a, b|
+    a.length <=> b.length 
+  end
+end
+
+def swap_elements(num)
+  num.sort do |a, b|
+    a[1] <=> b[2]
+  end
+end
+
+def reverse_array(num)
+  num.sort { |h, index| h[index.reverse] }
+end
+
 
